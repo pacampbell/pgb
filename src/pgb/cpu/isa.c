bf98867228260341024a125365949ab589ac6a19
Paul Campbell <pacampbell87@gmail.com>
2018-05-28 17:28:23 -0700

    Execution routing logic in place
    Connected the decoded opcodes to the functions which implement the
    functionality. All instructions are currently a NOP.

--- src/pgb/cpu/isa.c
+++ src/pgb/cpu/isa.c
@@ -74,7 +74,459 @@ int isa_prefix_cb_get_instruction(uint8_t opcode, struct isa_instruction **instr
 	return -EINVAL;
 }
 
-int isa_execute_instruction(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+static
+int isa_execute_instruction_adc(struct device *device, struct isa_instruction *instruction, uint8_t *data)
 {
 	return 0;
+}
+
+static
+int isa_execute_instruction_add(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_addc(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_and(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_call(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ccf(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_cp(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_cpl(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_daa(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_dec(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_di(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ei(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_halt(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_inc(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_jp(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_jr(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ld(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ldd(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ldh(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ldhl(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ldi(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_nop(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_or(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_pop(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_prefix(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_push(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_ret(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_reti(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rst(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_sbc(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_scf(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_stop(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_sub(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_xor(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_bit(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_res(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rl(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rla(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rlc(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rlca(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rr(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rra(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rrc(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_rrca(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_set(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_sla(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_sra(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_srl(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+static
+int isa_execute_instruction_swap(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	return 0;
+}
+
+int isa_execute_instruction(struct device *device, struct isa_instruction *instruction, uint8_t *data)
+{
+	int ret = 0;
+
+	switch (instruction->isa_operation) {
+	case ISA_OPERATION_ADC:
+		ret = isa_execute_instruction_adc(device, instruction, data);
+		break;
+	case ISA_OPERATION_ADD:
+		ret = isa_execute_instruction_add(device, instruction, data);
+		break;
+	case ISA_OPERATION_ADDC:
+		ret = isa_execute_instruction_addc(device, instruction, data);
+		break;
+	case ISA_OPERATION_AND:
+		ret = isa_execute_instruction_and(device, instruction, data);
+		break;
+	case ISA_OPERATION_CALL:
+		ret = isa_execute_instruction_call(device, instruction, data);
+		break;
+	case ISA_OPERATION_CCF:
+		ret = isa_execute_instruction_ccf(device, instruction, data);
+		break;
+	case ISA_OPERATION_CP:
+		ret = isa_execute_instruction_cp(device, instruction, data);
+		break;
+	case ISA_OPERATION_CPL:
+		ret = isa_execute_instruction_cpl(device, instruction, data);
+		break;
+	case ISA_OPERATION_DAA:
+		ret = isa_execute_instruction_daa(device, instruction, data);
+		break;
+	case ISA_OPERATION_DEC:
+		ret = isa_execute_instruction_dec(device, instruction, data);
+		break;
+	case ISA_OPERATION_DI:
+		ret = isa_execute_instruction_di(device, instruction, data);
+		break;
+	case ISA_OPERATION_EI:
+		ret = isa_execute_instruction_ei(device, instruction, data);
+		break;
+	case ISA_OPERATION_HALT:
+		ret = isa_execute_instruction_halt(device, instruction, data);
+		break;
+	case ISA_OPERATION_INC:
+		ret = isa_execute_instruction_inc(device, instruction, data);
+		break;
+	case ISA_OPERATION_JP:
+		ret = isa_execute_instruction_jp(device, instruction, data);
+		break;
+	case ISA_OPERATION_JR:
+		ret = isa_execute_instruction_jr(device, instruction, data);
+		break;
+	case ISA_OPERATION_LD:
+		ret = isa_execute_instruction_ld(device, instruction, data);
+		break;
+	case ISA_OPERATION_LDD:
+		ret = isa_execute_instruction_ldd(device, instruction, data);
+		break;
+	case ISA_OPERATION_LDH:
+		ret = isa_execute_instruction_ldh(device, instruction, data);
+		break;
+	case ISA_OPERATION_LDHL:
+		ret = isa_execute_instruction_ldhl(device, instruction, data);
+		break;
+	case ISA_OPERATION_LDI:
+		ret = isa_execute_instruction_ldi(device, instruction, data);
+		break;
+	case ISA_OPERATION_NOP:
+		ret = isa_execute_instruction_nop(device, instruction, data);
+		break;
+	case ISA_OPERATION_OR:
+		ret = isa_execute_instruction_or(device, instruction, data);
+		break;
+	case ISA_OPERATION_POP:
+		ret = isa_execute_instruction_pop(device, instruction, data);
+		break;
+	case ISA_OPERATION_PREFIX:
+		ret = isa_execute_instruction_prefix(device, instruction, data);
+		break;
+	case ISA_OPERATION_PUSH:
+		ret = isa_execute_instruction_push(device, instruction, data);
+		break;
+	case ISA_OPERATION_RET:
+		ret = isa_execute_instruction_ret(device, instruction, data);
+		break;
+	case ISA_OPERATION_RETI:
+		ret = isa_execute_instruction_reti(device, instruction, data);
+		break;
+	case ISA_OPERATION_RST:
+		ret = isa_execute_instruction_rst(device, instruction, data);
+		break;
+	case ISA_OPERATION_SBC:
+		ret = isa_execute_instruction_sbc(device, instruction, data);
+		break;
+	case ISA_OPERATION_SCF:
+		ret = isa_execute_instruction_scf(device, instruction, data);
+		break;
+	case ISA_OPERATION_STOP:
+		ret = isa_execute_instruction_stop(device, instruction, data);
+		break;
+	case ISA_OPERATION_SUB:
+		ret = isa_execute_instruction_sub(device, instruction, data);
+		break;
+	case ISA_OPERATION_XOR:
+		ret = isa_execute_instruction_xor(device, instruction, data);
+		break;
+	/* Start of prefix instructions */
+	case ISA_OPERATION_BIT:
+		ret = isa_execute_instruction_bit(device, instruction, data);
+		break;
+	case ISA_OPERATION_RES:
+		ret = isa_execute_instruction_res(device, instruction, data);
+		break;
+	case ISA_OPERATION_RL:
+		ret = isa_execute_instruction_rl(device, instruction, data);
+		break;
+	case ISA_OPERATION_RLA:
+		ret = isa_execute_instruction_rla(device, instruction, data);
+		break;
+	case ISA_OPERATION_RLC:
+		ret = isa_execute_instruction_rlc(device, instruction, data);
+		break;
+	case ISA_OPERATION_RLCA:
+		ret = isa_execute_instruction_rlca(device, instruction, data);
+		break;
+	case ISA_OPERATION_RR:
+		ret = isa_execute_instruction_rr(device, instruction, data);
+		break;
+	case ISA_OPERATION_RRA:
+		ret = isa_execute_instruction_rra(device, instruction, data);
+		break;
+	case ISA_OPERATION_RRC:
+		ret = isa_execute_instruction_rrc(device, instruction, data);
+		break;
+	case ISA_OPERATION_RRCA:
+		ret = isa_execute_instruction_rrca(device, instruction, data);
+		break;
+	case ISA_OPERATION_SET:
+		ret = isa_execute_instruction_set(device, instruction, data);
+		break;
+	case ISA_OPERATION_SLA:
+		ret = isa_execute_instruction_sla(device, instruction, data);
+		break;
+	case ISA_OPERATION_SRA:
+		ret = isa_execute_instruction_sra(device, instruction, data);
+		break;
+	case ISA_OPERATION_SRL:
+		ret = isa_execute_instruction_srl(device, instruction, data);
+		break;
+	case ISA_OPERATION_SWAP:
+		ret = isa_execute_instruction_swap(device, instruction, data);
+		break;
+	default:
+		ret = -EINVAL;
+		fprintf(stderr, "Encountered unexpected operation type '%02x'. (Emulator error?)\n", instruction->isa_operation);
+		break;
+	}
+	OK_OR_WARN(ret == 0);
+
+	return ret;
 }
