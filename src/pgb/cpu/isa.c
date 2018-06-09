ceb132310815b294c56e51695dc5766d362eb857
Paul Campbell <pacampbell87@gmail.com>
2018-06-08 20:52:54 -0700

    [decoder] Changed table decoder logic.
    Now that all opcodes are generated, opcode lookup is
    now a simple access with the opcode as the key to decode.

--- src/pgb/cpu/isa.c
+++ src/pgb/cpu/isa.c
@@ -4,12 +4,13 @@
 
 #include <pgb/cpu/isa.h>
 #include <pgb/cpu/private/isa.h>
+#include <pgb/cpu/private/isa_constants.h>
 #include <pgb/cpu/registers.h>
 #include <pgb/debug.h>
 #include <pgb/utils.h>
 
 static
-struct isa_instruction isa_instruction_set[] = {
+struct isa_instruction isa_instruction_set[LR35902_OPCODE_TABLE_SIZE] = {
 #define SET(MNEMONIC, ASSEMBLY, OPCODE, OPERATION_CLASS, OPERAND_A, MODIFIER_A, OPERAND_B, MODIFIER_B, NUM_BYTES, C0, C1, FLAG_MASK, FLAG_Z, FLAG_N, FLAG_H, FLAG_C, IS_PREFIX) \
 	{#MNEMONIC, ASSEMBLY, OPCODE, NUM_BYTES, {C0, C1}, {FLAG_MASK, FLAG_Z, FLAG_N, FLAG_H, FLAG_C}, IS_PREFIX, {OPERAND_A, OPERAND_B, MODIFIER_A, MODIFIER_B}, OPERATION_CLASS},
 #include <pgb/cpu/private/isa.def>
@@ -17,7 +18,7 @@ struct isa_instruction isa_instruction_set[] = {
 };
 
 static
-struct isa_instruction prefix_isa_instruction_set[] = {
+struct isa_instruction prefix_isa_instruction_set[LR35902_PREFIX_CB_OPCODE_TABLE_SIZE] = {
 #define SET(MNEMONIC, ASSEMBLY, OPCODE, OPERATION_CLASS, OPERAND_A, MODIFIER_A, OPERAND_B, MODIFIER_B, NUM_BYTES, C0, C1, FLAG_MASK, FLAG_Z, FLAG_N, FLAG_H, FLAG_C, IS_PREFIX) \
 	{#MNEMONIC, ASSEMBLY, OPCODE, NUM_BYTES, {C0, C1}, {FLAG_MASK, FLAG_Z, FLAG_N, FLAG_H, FLAG_C}, IS_PREFIX, {OPERAND_A, OPERAND_B, MODIFIER_A, MODIFIER_B}, OPERATION_CLASS},
 #include <pgb/cpu/private/prefix_cb.def>
@@ -26,38 +27,12 @@ struct isa_instruction prefix_isa_instruction_set[] = {
 
 int isa_get_instruction(uint8_t opcode, struct isa_instruction **instruction)
 {
-	int ret;
-	unsigned i;
-
-	// XXX: Make lookup faster
-	ret = -EINVAL;
-	for (i = 0; i < ARRAY_SIZE(isa_instruction_set); i++) {
-		if (opcode == isa_instruction_set[i].opcode) {
-			*instruction = &isa_instruction_set[i];
-			ret = 0;
-			break;
-		}
-	}
-	OK_OR_WARN(ret == 0);
-
-	return ret;
+	*instruction = &isa_instruction_set[opcode];
+	return 0;
 }
 
 int isa_prefix_cb_get_instruction(uint8_t opcode, struct isa_instruction **instruction)
 {
-	int ret;
-	unsigned i;
-
-	// XXX: Make lookup faster
-	ret = -EINVAL;
-	for (i = 0; i < ARRAY_SIZE(prefix_isa_instruction_set); i++) {
-		if (opcode == prefix_isa_instruction_set[i].opcode) {
-			*instruction = &prefix_isa_instruction_set[i];
-			ret = 0;
-			break;
-		}
-	}
-	OK_OR_WARN(ret == 0);
-
-	return ret;
+	*instruction = &prefix_isa_instruction_set[opcode];
+	return 0;
 }
